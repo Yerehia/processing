0fa0957e39b8e38ddc864742e0a2ae1df266fa48
Andrea <andangrei@alum.us.es>
2024-04-27 17:36:37 +0200

    Update eventHandler.js

--- eventHandler.js
+++ eventHandler.js
@@ -1,34 +1,34 @@
-function assignEvent(p5Element, eventType, newScript, customJS) {
+function assignEvent(p5Element, eventType, customJS, ...newScriptActions) {
     switch (eventType) {
         case 'MOUSE_CLICK':
-            p5Element.mouseClicked(() => { sendEvent(newScript, customJS); });
+            p5Element.mouseClicked(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'DOUBLE_CLICKED':
-            p5Element.doubleClicked(() => { sendEvent(newScript, customJS); });
+            p5Element.doubleClicked(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'MOVED':
-            p5Element.mouseMoved(() => { sendEvent(newScript, customJS); });
+            p5Element.mouseMoved(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'DRAGGED':
-            p5Element.mouseDragged(() => { sendEvent(newScript, customJS); });
+            p5Element.mouseDragged(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'MOUSE_PRESSED':
-            p5Element.mousePressed(() => { sendEvent(newScript, customJS); });
+            p5Element.mousePressed(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'MOUSE_RELEASED':
-            p5Element.mouseReleased(() => { sendEvent(newScript, customJS); });
+            p5Element.mouseReleased(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'SCROLLED':
-            p5Element.mouseWheel(() => { sendEvent(newScript, customJS); });
+            p5Element.mouseWheel(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'KEY_PRESSED':
-            p5Element.keyPressed(() => { sendEvent(newScript, customJS); });
+            p5Element.keyPressed(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'KEY_RELEASED':
-            p5Element.keyReleased(() => { sendEvent(newScript, customJS); });
+            p5Element.keyReleased(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         case 'KEY_TYPED':
-            p5Element.keyTyped(() => { sendEvent(newScript, customJS); });
+            p5Element.keyTyped(() => { sendEvent(customJS, ...newScriptActions); });
             break;
         default:
             console.error("Tipo de evento no reconocido");
@@ -36,7 +36,7 @@ function assignEvent(p5Element, eventType, newScript, customJS) {
     }
 }
 
-function sendEvent(newScript, customJS) {
+function sendEvent(customJS, ...newScriptActions) {
     var xhr = new XMLHttpRequest();
 
     xhr.open("POST", "/", true);
@@ -44,7 +44,8 @@ function sendEvent(newScript, customJS) {
 
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
-            eval(xhr.responseText);
+            var result = JSON.parse(xhr.responseText);
+            result.forEach(action => eval(action));
          } else {
              console.error('La solicitud falló con el código de estado: ' + xhr.status);
          }
@@ -53,8 +54,7 @@ function sendEvent(newScript, customJS) {
     xhr.onerror = function() {
         console.error('La solicitud falló');
     };
-    console.log(newScript);
-    xhr.send("newScriptContent=" + encodeURIComponent(JSON.stringify(newScript)));
+    xhr.send("newScriptContent="+ encodeURIComponent(JSON.stringify(newScriptActions)));
 
     eval(customJS)
 }
