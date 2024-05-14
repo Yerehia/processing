82b4adf02909e356d0e6cd32fe7f434fcef79630
Andrea <andangrei@alum.us.es>
2024-05-14 19:48:41 +0200

    Create input.js

---
+++ input.js
@@ -0,0 +1,42 @@
+class CustomInput {
+  constructor(x, y, width, height, placeholder, customClass) {
+    // Creamos el inp usando p5.js
+    this.inp = createInput();
+
+    // Establecemos la posición y el tamaño del inp
+    if (x != null && y != null) {
+      this.inp.position(x, y);
+    } else {
+      this.inp.style('position', '');
+      this.inp.style('left', '');
+      this.inp.style('top', '');
+    }
+    if(width != null && height != null) {
+        this.btn.size(width, height);
+    }
+
+    // Establecemos el placeholder del inp
+    if (placeholder != null) {
+      this.inp.attribute('placeholder', placeholder);
+    }
+
+    // Aplicamos la clase personalizada
+    this.inp.addClass(customClass);
+  }
+
+  // Método para establecer la clase personalizada
+  setCustomClass(customClass) {
+    this.inp.removeClass();
+    this.inp.addClass(customClass);
+  }
+
+  // Método para establecer el valor del inp
+  setValue(value) {
+    this.inp.value(value);
+  }
+
+  // Método para obtener el valor del inp
+  getValue() {
+    return this.inp.value();
+  }
+}
