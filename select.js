563b0d09d55d7ab2e7107742662e6eaa9de340f2
Andrea <andangrei@alum.us.es>
2024-05-14 19:37:22 +0200

    Update select.js

--- select.js
+++ select.js
@@ -1,44 +1,43 @@
 class CustomSelect {
   constructor(x, y, width, heigh, options, customClass) {
-    // Creamos el select usando p5.js
-    this.select = createSelect();
+    // Creamos el stc usando p5.js
+    this.stc = createSelect();
 
-    // Establecemos la posición y el tamaño del select
+    // Establecemos la posición y el tamaño del stc
     if (x != null && y != null) {
-      this.select.position(x, y);
+      this.stc.position(x, y);
     } else {
-      this.select.style('position', '');
-      this.select.style('left', '');
-      this.select.style('top', '');
+      this.stc.style('position', '');
+      this.stc.style('left', '');
+      this.stc.style('top', '');
     }
     
     if(width != null && height != null) {
         this.btn.size(width, height);
     }
 
-    console.log(options);
-    // Agregamos opciones al select
+    // Agregamos opciones al stc
     for (let option of options) {
-      this.select.option(option);
+      this.stc.option(option);
     }
 
     // Aplicamos la clase personalizada
-    this.select.addClass(customClass);
+    this.stc.addClass(customClass);
   }
 
   // Método para establecer la clase personalizada
   setCustomClass(customClass) {
-    this.select.removeClass();
-    this.select.addClass(customClass);
+    this.stc.removeClass();
+    this.stc.addClass(customClass);
   }
 
-  // Método para establecer el valor seleccionado del select
+  // Método para establecer el valor seleccionado del stc
   setSelected(value) {
-    this.select.value(value);
+    this.stc.value(value);
   }
 
-  // Método para obtener el valor seleccionado del select
+  // Método para obtener el valor seleccionado del stc
   getSelected() {
-    return this.select.value();
+    return this.stc.value();
   }
 }
