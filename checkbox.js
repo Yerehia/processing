e12ba2f6152e4603bcd329028d6ff1f747ad2129
Andrea <andangrei@alum.us.es>
2024-05-14 20:33:32 +0200

    Update checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -1,5 +1,5 @@
 class CustomCheckbox {
-  constructor(x, y, width, height, checked, customClass) {
+  constructor(x, y, width, height, checked, customClass, parentGroupClass) {
     // Creamos el box usando p5.js
     if(checked) {
       this.box = createCheckbox(true);
@@ -19,9 +19,14 @@ class CustomCheckbox {
       this.box.size(width, height);
     }
 
-    // Aplicamos la clase personalizada
-    console.log(this.box.child());
-    this.box.addClass(customClass);
+    // Aplicamos las clases personalizadas
+    if(parentGroupClass != null) {
+      this.box.addClass(parentGroupClass);
+    }
+
+    if(customClass != null) {
+      this.box.child()[0].classList.add(customClass);
+    }
 
     this.box.changed(() => {
       this.box.checked() ? this.box.attribute('checked', 'checked') : this.box.removeAttribute('checked');
