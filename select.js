d8405eaf6e102dddfedcf98ae13fd84a996fa3a9
Andrea <andangrei@alum.us.es>
2024-05-14 19:19:04 +0200

    Create select.js

---
+++ select.js
@@ -0,0 +1,42 @@
+class CustomSelect {
+  constructor(x, y, width, options, customClass) {
+    // Creamos el select usando p5.js
+    this.select = createSelect();
+
+    // Establecemos la posición y el tamaño del select
+    if (x != null && y != null) {
+      this.select.position(x, y);
+    } else {
+      this.select.style('position', '');
+      this.select.style('left', '');
+      this.select.style('top', '');
+    }
+    if (width != null) {
+      this.select.size(width);
+    }
+
+    // Agregamos opciones al select
+    for (let option of options) {
+      this.select.option(option);
+    }
+
+    // Aplicamos la clase personalizada
+    this.select.addClass(customClass);
+  }
+
+  // Método para establecer la clase personalizada
+  setCustomClass(customClass) {
+    this.select.removeClass();
+    this.select.addClass(customClass);
+  }
+
+  // Método para establecer el valor seleccionado del select
+  setSelected(value) {
+    this.select.value(value);
+  }
+
+  // Método para obtener el valor seleccionado del select
+  getSelected() {
+    return this.select.value();
+  }
+}
