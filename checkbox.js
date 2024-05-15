b94e4d7be8ed29febc8dc771ec4ad8cf5606656c
yerehia <ara.uni98@gmail.com>
2024-05-15 19:48:28 +0200

    fix checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -2,7 +2,7 @@ class CustomCheckbox {
   constructor(id, x, y, width, height, checked, customClass) {
     // Creamos el box usando p5.js
     if(checked) {
-      this.box = createCheckbox(true);
+      this.box = createCheckbox(null, true);
     } else {
       this.box = createCheckbox();
     }
@@ -20,8 +20,15 @@ class CustomCheckbox {
       this.box.size(width, height);
     }
 
-    // Aplicamos la clase personalizada
-    this.box.child()[0].addClass(customClass);
+
+    // Aplicamos las clases personalizadas
+    if(parentGroupClass != null) {
+      this.box.addClass(parentGroupClass);
+    }
+
+    if(customClass != null) {
+      this.box.child()[0].classList.add(customClass);
+    }
 
     this.box.changed(() => {
       this.box.checked() ? this.box.attribute('checked', 'checked') : this.box.removeAttribute('checked');
