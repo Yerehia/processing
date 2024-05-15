f4950be0f50ae8009abecbd0beecc4150767b20f
Andrea <andangrei@alum.us.es>
2024-05-15 18:56:40 +0200

    Create label.js

---
+++ label.js
@@ -0,0 +1,33 @@
+class CustomLabel {
+  constructor(x, y, width, height, text, reference, customClass) {
+    // Creamos el lbl usando p5.js
+    this.lbl = = createElement('label', text);
+
+    // Establecemos la posición y el tamaño del lbl
+    if (x != null && y != null) {
+      this.lbl.position(x, y);
+    } else {
+      this.lbl.style('position', '');
+      this.lbl.style('left', '');
+      this.lbl.style('top', '');
+    }
+    if(width != null && height != null) {
+        this.btn.size(width, height);
+    }
+
+    // Establecemos el placeholder del lbl
+    if (reference != null) {
+      this.lbl.attribute('for', reference);
+    }
+
+    // Aplicamos la clase personalizada
+    this.lbl.addClass(customClass);
+  }
+
+  // Método para establecer la clase personalizada
+  setCustomClass(customClass) {
+    this.lbl.removeClass();
+    this.lbl.addClass(customClass);
+  }
+
+}
